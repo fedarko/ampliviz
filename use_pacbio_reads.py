5262230ed820dc4841c20c6e1a17b3d6e333d656
Marcus Fedarko <mfedarko@ucsd.edu>
2019-03-21 16:39:58 -0700

    ENH: Simplify real esv computations
    This keeps track of both the start and end of each reference
    chromosome, and doesn't bother distinguishing between if the start/end
    of an edge are on the same chromosome or not (it looks at the
    chromosomes of the start and end position regardless).
    
    This had a few ramifications on how certain support values for
    corner-case-ish edges were calculated. I think this is a step in
    the right direction, though.
    
    Would be good to add test code for this, if I can scrounge up a combo of
    publicly available + free PacBio and AmpliconArchitect data

--- use_pacbio_reads.py
+++ use_pacbio_reads.py
@@ -14,17 +14,15 @@ def read_pacbio_bam(pacbio_bam, bp_edges_list):
     # the SQ header to get the IDs
     seq_ids = [seq_header['SN'] for seq_header in pf.header["SQ"]]
     short_id2seq_id = {}
-    long_id2seq_start = {}
+    long_id2seq_bounds = {}
     for long_id in seq_ids:
         assert ':' in long_id
         short_id2seq_id[long_id.split(':')[0].lower()] = long_id
-        # Assumes the chromosome ID is of the form
+        # Assumes the reference chromosome ID is of the form
         # "chr1:abc-def", where abc is the observed start position of the
         # chromosome and def is the observed end position of the chromosome.
-        # This should work even if the entire chromosome is used here, since
-        # it'd have a chrstart value of 0
-        chrstart = int(long_id.split(':')[1].split('-')[0])
-        long_id2seq_start[long_id] = chrstart
+        bounds = [int(c) for c in long_id.split(':')[1].split('-')]
+        long_id2seq_bounds[long_id] = bounds
 
     edge2support = {}
     for e in bp_edges_list:
@@ -36,30 +34,28 @@ def read_pacbio_bam(pacbio_bam, bp_edges_list):
         end_chr_format = e.end_pos.chromosome.lower().replace('_', '.')
         start_chromosome = short_id2seq_id[start_chr_format]
         end_chromosome = short_id2seq_id[end_chr_format]
-        # This doesn't really guarantee support *between* the nodes, but I
-        # guess it's a reasonable approximation
-        if start_chromosome == end_chromosome:
-            # The coordinates are relative to the start of the chromosome,
-            # which might be set to something after 0 (so we have to adjust
-            # them)
-            chrstart = long_id2seq_start[start_chromosome]
-            start = min(e.start_pos.pos - chrstart, e.end_pos.pos - chrstart)
-            end = max(e.start_pos.pos - chrstart, e.end_pos.pos - chrstart)
-            # print(start, end)
-            f = pf.fetch(start_chromosome, start, end)
-            supporting_reads = set([r.query_name for r in f])
-        else:
-            chr1start = long_id2seq_start[start_chromosome]
-            chr2start = long_id2seq_start[end_chromosome]
-            f1 = pf.fetch(start_chromosome,
-                          e.start_pos.pos - chr1start,
-                          e.start_pos.pos - chr1start + 1)
-            f2 = pf.fetch(end_chromosome,
-                          e.end_pos.pos - chr2start - 1,
-                          e.end_pos.pos - chr2start)
-            r1 = [r.query_name for r in f1]
-            r2 = [r.query_name for r in f2]
-            supporting_reads = set(r1) & set(r2)
+        # This doesn't really guarantee support *between* the nodes, but it's
+        # a reasonable approximation
+        chr1bounds = long_id2seq_bounds[start_chromosome]
+        chr2bounds = long_id2seq_bounds[end_chromosome]
+        assert chr1bounds[1] - chr1bounds[0] > 1
+        assert chr2bounds[1] - chr2bounds[0] > 1
+        edge_start = e.start_pos.pos - chr1bounds[0]
+        edge_end = e.end_pos.pos - chr2bounds[0]
+        # print(e.start_pos.pos, chr1bounds, e.end_pos.pos, chr2bounds)
+        # From testing, I don't *think* this will cause problems if edge_start
+        # is located at the last possible position on a reference chromosome.
+        f1 = pf.fetch(start_chromosome, edge_start, edge_start + 1)
+        # ...However, if edge_end is 0, that will be a problem -- since -1 is
+        # an invalid coordinate. We compensate for this by using (0, 1) as the
+        # f2 region in that case (since we've asserted that these chromosomes
+        # have at least two base pairs, we should be fine).
+        if edge_end == 0:
+            edge_end += 1
+        f2 = pf.fetch(end_chromosome, edge_end - 1, edge_end)
+        r1 = [r.query_name for r in f1]
+        r2 = [r.query_name for r in f2]
+        supporting_reads = set(r1) & set(r2)
         # Use of set() means that split reads (i.e. with same query name) will
         # only be counted as one, which is perfect.
         # Also, since we only look at intersection of r1 and r2, we don't care
